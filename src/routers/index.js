49e433983837482f959c4e0781be75b45eb0e0fc
Gagik-arch <gagik.chilingaryan2020@gmail.com>
2022-06-11 18:59:09 +0400

    make it better

--- src/routers/index.js
+++ src/routers/index.js
@@ -8,7 +8,6 @@ const router = Router()
 
 router.get("/api/restaurants/getAll", RestaurantController.getAll)
 router.post("/api/restaurants/add", RestaurantController.add)
-router.get("/api/restaurants/clear", RestaurantController.clear)
-router.get("/", RestaurantController.clear)
+router.get('*', (_, res) => res.status(404).json({error:'Page not found'}))
 
 export default router
