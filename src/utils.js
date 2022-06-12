d5ca6aa9a22523dfe50b1e98014ba78897ce3010
Gagik-arch <gagik.chilingaryan2020@gmail.com>
2022-06-12 23:45:25 +0400

    asd

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 export const getAverageRating = (reviews) => {
     let rating = 0
     reviews.forEach(item => rating += item.rating)
-    return Math.floor(rating / reviews.length);
+    return Math.round(rating / reviews.length);
 }
