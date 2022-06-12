5a6e4ad0d1b169d8a71454c6ed912ac85cf6c33e
Gagik-arch <gagik.chilingaryan2020@gmail.com>
2022-06-13 00:21:31 +0400

    asdad

--- src/routers/index.js
+++ src/routers/index.js
@@ -9,7 +9,7 @@ router.get("/api/restaurants/getAll", RestaurantController.getAll)
 router.post("/api/restaurants/add", RestaurantController.add)
 router.post("/api/restaurants/getOne", RestaurantController.getRestaurant)
 router.post("/api/restaurants/feedback", RestaurantController.sendFeedback)
-router.post("/api", (res)=>{
+router.post("/", (res)=>{
     res.send('App is running')
 })
 router.get('*', (_, res) => res.status(404).json({error: 'Page not found'}))
